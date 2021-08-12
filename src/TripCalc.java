62c1f5b710b1fa120a118ec8adaf5955b6e1d38d
tj <tjtrolling@gmail.com>
2021-08-11 21:24:28 -0700

    Added function signature

--- src/TripCalc.java
+++ src/TripCalc.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripCalc {
+    
+    List<String> route (String starting_city, String ending_city, List<String> attractions){
+        //TODO
+        return null;
+    }
+
 }
